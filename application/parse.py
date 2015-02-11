116ef92a7769011faa20939c1704ecdcaf132056
Jonathan Girven <jonathan.girven@trickbook.com>
2015-02-11 15:17:13 +0000

    Fix for missing items - Check no JS version of page

--- application/parse.py
+++ application/parse.py
@@ -14,35 +14,57 @@ from flask import url_for
 from application import app
 from application.model import Item
 
-URL = "http://www.amazon.co.uk/s/?me=A2OAJ7377F756P&rh=i%3Amerchants%2Cn%3A429892031&rw_html_to_wsrp=1"
+URL_PREFIX = "http://www.amazon.co.uk/s/ref=sr_pg_2?me=A2OAJ7377F756P&rh=n%3A340831031%2Cn%3A%21340832031%2Cn%3A429892031&page="
+URL_SUFFIX = "&ie=UTF8&qid=1423666390"
 
 def parse_sites():                                                                
     """Parse sites"""
-    req = urllib2.Request(URL)
-    req.add_header('User-agent', 'Mozilla/5.0\
- (Windows NT 6.2; WOW64) AppleWebKit/537.11 (KHTML, like Gecko)\
- Chrome/23.0.1271.97 Safari/537.11')
-    response = urllib2.urlopen(req)
-    html = response.read()
-    
-    soup = BeautifulSoup(html)
-    
-    ul = soup.find("ul", id="s-results-list-atf")
-    for li in ul.find_all('li'):
-        title = li.find("h2", "s-access-title").text
-        spans = li.find_all("span", "a-color-price")
-        price = spans[0].text
+    page = 1
+    items = 0
 
-        item = Item(
-                title=title,
-                price=convert_price(price)
-            )
-        item.put()
+    while True:
+        logging.info("Page: %s" % page)
 
-        logging.info("Item created: %s" % ( item.title))
+        # Send request
+        req = urllib2.Request(URL_PREFIX + str(page) + URL_SUFFIX)
+        req.add_header('User-agent', 'Mozilla/5.0\
+ (Wi    ndows NT 6.2; WOW64) AppleWebKit/537.11 (KHTML, like Gecko)\
+ Chr    ome/23.0.1271.97 Safari/537.11')
+        response = urllib2.urlopen(req)
+        html = response.read()
+        
+        # Parse response
+        soup = BeautifulSoup(html)
+
+        # Check if we went passed the max pages
+        currentPage = int(soup.find('span', 'pagnCur').text)
+        if currentPage < page:
+            logging.info("Current page: %s" % currentPage)
+            break
+       
+        # Iterate through ul's and li's looking for items
+        for ul in soup.find_all("ul", "s-result-list"):
+            for li in ul.find_all('li'):
+                # Grab info
+                title = li.find("h2", "s-access-title").text
+                spans = li.find_all("span", "a-color-price")
+                price = spans[0].text
+
+                # Save to DB
+                item = Item(
+                        title=title,
+                        price=convert_price(price)
+                    )
+                item.put()
+
+                items += 1
+
+                logging.info("Item created: %s" % ( item.title))
+
+        page += 1
 
 
-    return html
+    return "Scanned %s pages, found %s items" % (page, items)
 
 
 def convert_price(price):
